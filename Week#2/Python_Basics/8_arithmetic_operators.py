5177c702f46debaa7f7529c9b4011e8d03e39e99
Ejder Aysun <ejderaysunn@gmail.com>
2023-03-26 00:49:23 +0300

    week 2, python basics, 8. topic: arithmetic operators added

---
+++ Week#2/Python_Basics/8_arithmetic_operators.py
@@ -0,0 +1,57 @@
+a = 4
+b = 3
+print(-b)
+# Output: -3
+
+print(a + b)
+# Output: 7
+
+print(a / b)
+# Output: 1.3333333333333333
+
+print(a % b)
+# Output: 1
+
+print(a**b)
+# Output: 64
+
+print(10 / 5)
+# Output: 2.0
+
+print(type(10/5))
+# Output: <class 'float'>
+
+print(2 * 25 + 30)
+# Output: 80
+
+'''
+
+Operator        Example        Meaning             Result
+
++ (unary)       +a             Unary Positive      a
+                                                   In other words, it doesnt
+                                                   really do anything. It mostly
+                                                   exists for the sake of
+                                                   completeness, to complement
+                                                   Unary Negation
+
++ (binary)      a + b          Addition            Sum of a and b
+
+- (unary)       -a             Unary Negation      Value equal to a but opposite in sign
+
+- (binary)      a - b          Subtraction         b subtracted from a
+
+*               a * b          Multiplication      Product of a and b
+
+/               a / b          Division            Quotient when a is divided by b.
+                                                   The result always has type float.
+
+%               a % b          Modulo              Remainder when a is divided by b
+
+//              a // b         Floor Division      Quotient when a is divided by b,
+                               (also called        rounded to the next smallest whole number
+                               Integer Division)
+
+**              a ** b         Exponentiation      a raised to the power of b
+
+'''
