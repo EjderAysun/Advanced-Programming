f139f1a60ea4f7b8f0b3d0df651f8ad1751ac1b1
Ejder Aysun <ejderaysunn@gmail.com>
2023-03-26 00:55:22 +0300

    week 2, python basics, 9. topic: comparison operators added

---
+++ Week#2/Python_Basics/9_comparison_operators.py
@@ -0,0 +1,45 @@
+a = 10
+b = 20
+print(a == b)
+# Output: False
+
+print(a != b)
+# Output: True
+
+print(a <= b)
+# Output: True
+
+print(a >= b)
+# Output: False
+
+a = 10
+b = 10
+print(a == b)
+# Output: True
+
+print(type(a == b))
+# Output: <class 'bool'>
+
+'''
+
+Operator     Example     Meaning                    Result
+
+==           a == b      Equal to                   True if the value of a is equal to the value of b
+                                                    False otherwise
+
+!=           a != b      Not equal to               True if a is not equal to b
+                                                    False otherwise
+
+<            a < b       Less than                  True if a is less than b
+                                                    False otherwise
+
+<=           a <= b      Less than or equal to      True if a is less than or equal to b
+                                                    False otherwise
+
+>            a > b       Greater than               True if a is greater than b
+                                                    False otherwise
+
+>=           a >= b      Greater than or equal to   True if a is greater than or equal to b
+                                                    False otherwise
+
+'''
