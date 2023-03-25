cbf6f58a606fd99de9b6ca2909fb71b4140db771
Ejder Aysun <ejderaysunn@gmail.com>
2023-03-25 22:04:34 +0300

    week 2, cpp basics, 1. topic: hello universe added

---
+++ Week#2/CPP_Basics/1_hello_universe.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// C++ actually adds an extra step to the basic compilation
+// process: the code is run through a preprocessor,
+// which applies some modifications to the source code,
+// before being fed to the compiler.
+
+int main() {
+    cout<<"Hello Universe";
+    return 0;
+}
