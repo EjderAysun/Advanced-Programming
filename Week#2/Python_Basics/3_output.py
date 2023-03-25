04abf3e984b365c39c5fce0e4e8bc60cb686d026
Ejder Aysun <ejderaysunn@gmail.com>
2023-03-25 22:18:45 +0300

    week 2, python basics, 3. topic: output added

---
+++ Week#2/Python_Basics/3_output.py
@@ -0,0 +1,15 @@
+# This is the syntax for outputting some piece of text to the screen:
+print("This string will be printed out")
+
+###########
+
+# Indentation
+# Python uses indentation for blocks, instead of curly braces.
+# Both tabs and spaces are supported, but standard indentation is four spaces,
+# and using standard indentation requires standard python code.
+x = 1
+if x==1:
+    # indented four spaces
+    print("x is 1")
+else:
+    print("x is not 1")
