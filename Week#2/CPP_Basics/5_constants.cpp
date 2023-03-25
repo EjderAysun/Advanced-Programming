aec0c1a174a918cebce4a80797d925fae987d4b0
Ejder Aysun <ejderaysunn@gmail.com>
2023-03-25 22:27:32 +0300

    week 2, cpp basics, 5. topic: constants added

---
+++ Week#2/CPP_Basics/5_constants.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // C++: String constant use double quotes ("), characters use single quotes (').
+    cout<<123<<endl;  // output: 123
+    cout<<98.6<<endl;  // output: 98.6
+    cout<<"Hello Universe";  // output: Hello Universe
+
+    return 0;
+}
