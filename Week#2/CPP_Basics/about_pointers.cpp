7110d1de82936d730f8cf4ebc11470185a74f148
Ejder Aysun <ejderaysunn@gmail.com>
2023-03-26 01:02:33 +0300

    week 2, cpp basics, extra: about pointers added

---
+++ Week#2/CPP_Basics/about_pointers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Pointer initialized with NULL
+    // Then request memory for the varaible
+    int *p = NULL;
+    p = new int;
+    *p = 6;
+    cout<<*p<<endl;
+    cout<<p<<endl;
+
+    // Combine declaration of pointer
+    // and their assignment
+    int *q = new int;
+    
+    return 0;
+
+    // In following weeks, we will learn the details on new and delete operators and pointer!!
+}
