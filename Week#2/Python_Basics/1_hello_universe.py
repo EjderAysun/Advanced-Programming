ff46412ef39f8e2ee46652695809b15320ddfadb
Ejder Aysun <ejderaysunn@gmail.com>
2023-03-25 22:05:38 +0300

    week 2, python basics, 1. topic: hello universe added

---
+++ Week#2/Python_Basics/1_hello_universe.py
@@ -0,0 +1,11 @@
+'''
+In Python no (not always) need to store source code in a file → INTERPRETED!
+
+Python Interpretation
+- Python requires a piece of software called an interpreter.
+- The interpreter is the program you will need to run Python code and scripts.
+- The interpreter is able to run Python code in two different ways:
+    1. As a script model
+    2. As a piece of code typed into an interactive session (Three arrows is the prompt of the Python interpreter.)
+'''
+print("Hello Universe")
