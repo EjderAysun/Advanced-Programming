dff7639345c7160b80067e3a524e6cc4855a881e
Ejder Aysun <ejderaysunn@gmail.com>
2023-03-25 22:11:15 +0300

    week 2, cpp basics, 2. topic: comments added

---
+++ Week#2/CPP_Basics/2_comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// Single-line comments (informally, C++ style), start with // and continue until the end of the line.
+// If the last character in a comment line is a \
+the comment will continue in the next line.
+
+    cout<<"Hello Universe"<<endl;  // This is also comment line, too.
+
+/*
+Multi-line comments (informally, C style), start with /* and end  with * /
+*/
+}
