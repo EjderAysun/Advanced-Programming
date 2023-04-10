f2d11d43741ca5578ec555984f99297a4390727d
Ejder Aysun <ejderaysunn@gmail.com>
2023-04-10 08:30:59 +0300

    some edits made on 'comments' + 'general comment system' introduced

--- Week#2/CPP_Basics/2_comments.cpp
+++ Week#2/CPP_Basics/2_comments.cpp
@@ -6,9 +6,46 @@ int main() {
 // If the last character in a comment line is a \
 the comment will continue in the next line.
 
-    cout<<"Hello Universe"<<endl;  // This is also comment line, too.
+    cout<<"Hello Universe"<<endl;  // This is also a comment line.
 
 /*
-Multi-line comments (informally, C style), start with /* and end  with * /
+Multi-line comments (informally, C style),
+start with '/*' and end with '* /'.
 */
+
+// How will my system of using comment lines be? //
+
+// - I will use the '//' sign to indicate items/comments.
+// - I will use the '-' sign to indicate items.
+// - If comment is not an item, there will be ' '(one space) sign at the beginning. \
+Paragraph, carriage return, etc. the number of spaces may vary according to other grammatical \
+features such as \
+(If it is the beginning of a paragraph, 4 spaces if it is a sub-item, 4 spaces + '-' sign, etc.)
+// - When my item/comment is too long for a single line, \
+I will use the '\' sign at the end of the item/comment \
+to indicate the rest of the item/comment.
+// - If an item/comment refers to a short line of code; \
+I will write the line of code as a continuation of the item/comment, that is, I will use '\' sign.
+// - If the code is long or short but independent of any clauses, \
+I will use the '/**/' sign to denote the code.
+// - Titles will be specified as '// Title //'.
+// - For very long texts, tables, drawings at the command prompt, \
+and some other special operations that may happen, \
+I will use the '/**/' sign to avoid confusion.
+
+
+// A usage example describing the whole system: //
+
+// Mottos //
+// Mens, ratio et intelligentia nostra agendi principia sunt. \
+Omnia facta quae vitam nostram complent huius veritatis testimonia sunt.
+// Haec ego non multis (scribo), sed tibi: \
+    cout<<"satis enim magnum alter alteri theatrum sumus."<<endl;
+/*
+    while(true): {
+        cout<<"Flectere si nequeo superos, Acheronta movebo."<<endl;
+    }
+*/
+// - Pulvis et umbra sumus.
+// - In soluto problema vivere stultis est proprium.
 }
--- Week#2/Python_Basics/2_comments.py
+++ Week#2/Python_Basics/2_comments.py
@@ -1,2 +1,45 @@
-# Anything after a # is ignored by Python
-print("Hello Universe")  # This is comment line, too
+# Anything after a # is ignored by Python.
+print("Hello Universe")  # This is also a comment line.
+'''
+This
+usage
+is
+for
+multiple
+comment
+lines
+'''
+
+# How will my system of using comment lines be? #
+
+# - I will use the '#' sign to indicate comments.
+# - I will use the '-' sign to indicate items.
+# - If comment is not an item, there will be ' '(one space) sign at the beginning. \
+# Paragraph, carriage return, etc. the number of spaces may vary according to other grammatical \
+# features such as \
+# (If it is the beginning of a paragraph, 4 spaces if it is a sub-item, 4 spaces + '-' sign, etc.)
+# - When my item/comment is too long for a single line, \
+# I will use the '\' sign at the end of the item/comment \
+# to indicate the rest of the item/comment.
+# - I will always use the `'''...'''` sign to denote the code(s).
+# - Titles will be specified as '# Title #'.
+# - For very long texts, tables, drawings at the command prompt, \
+# and some other special operations that may happen, \
+# I will use the `'''...'''` sign to avoid confusion.
+
+
+# A usage example describing the whole system: #
+
+# Mottos #
+# Mens, ratio et intelligentia nostra agendi principia sunt. \
+# Omnia facta quae vitam nostram complent huius veritatis testimonia sunt.
+# Haec ego non multis (scribo), sed tibi:
+'''
+print("satis enim magnum alter alteri theatrum sumus.")
+'''
+'''
+while True:
+    print("Flectere si nequeo superos, Acheronta movebo.")
+'''
+# - Pulvis et umbra sumus.
+# - In soluto problema vivere stultis est proprium.
